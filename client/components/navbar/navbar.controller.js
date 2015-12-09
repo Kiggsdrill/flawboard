e15d81f19360cb51ebcbbd1744e2cff86b236693
kiggsdrill <kiggsdrill@gmail.com>
2015-12-09 16:50:44 +0100

    Evolutions

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -2,25 +2,42 @@
 
 class NavbarController {
   //start-non-standard
-  menu = [{
-    'title': 'Home',
-    'link': '/'
+
+  this.messages=[{
+    author:'Shakira',
+    message: 'Whenever wherever!',
+    date: 'Today'
+  },{
+    author:'Rihanna',
+    message: 'Bitch better have my money!',
+    date: 'Today'
+
   }];
 
   isCollapsed = true;
   //end-non-standard
 
-  constructor($locationAuth) {
+  constructor($location, Auth) {
     this.$location = $location;
     this.isLoggedIn = Auth.isLoggedIn;
     this.isAdmin = Auth.isAdmin;
-    this.getCurrentUser = Auth.getCurrentUser;
+    this.getCurrentUser = Auth.getCurrentUser; 
   }
 
   isActive(route) {
     return route === this.$location.path();
   }
+
+  add(){
+    this.messages.push({
+      author:'Britney',
+      message: 'LOL',
+      date: 'Today'
+
+    });
+  }
+
 }
 
 angular.module('flawboardApp')
-  .controller('NavbarController', NavbarController);
+.controller('NavbarController', NavbarController);
